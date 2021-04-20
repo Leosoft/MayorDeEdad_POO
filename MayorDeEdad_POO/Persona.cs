c0e5f744ae6805da02eca0f8a4e6ca24a3455e04
Leosoft <leosoft3031@gmail.com>
2021-04-20 13:36:41 -0300

    Agregar archivos de proyecto.

---
+++ MayorDeEdad_POO/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorDeEdad_POO
+{
+    class Persona
+    {
+        public string nombre = "Pedro Picapiedra";
+        public int edad;
+
+        
+
+        public bool ComprobarEdad(int valoredad)
+        {
+
+            edad = valoredad;
+            if(edad > 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MayorDeEdad_POO/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MayorDeEdad_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Persona per = new Persona();
+            int edadactual = 0;
+
+
+            Console.WriteLine("Ingrese su edad: ");
+            edadactual = Convert.ToInt16(Console.ReadLine());
+            bool condi = per.ComprobarEdad(edadactual);
+            Console.WriteLine("Nombre: {0}", per.nombre);
+            Console.WriteLine("Edad: " + per.edad);
+
+            if (condi == true)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
